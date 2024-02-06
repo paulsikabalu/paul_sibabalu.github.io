1d44bd985b0632be719e4e521104b037dd8287c7
paulsikabalu <sikabalu@outlook.com>
2024-02-06 18:18:19 +0200

    portifolio

--- js/theme.js
+++ js/theme.js
@@ -9,16 +9,7 @@
 
 	//* Navbar Fixed  
     function navbarFixed(){
-        if ( $('.header_area').length ){ 
-            $(window).scroll(function() {
-                var scroll = $(window).scrollTop();   
-                if (scroll >= nav_offset_top ) {
-                    $(".header_area").addClass("navbar_fixed");
-                } else {
-                    $(".header_area").removeClass("navbar_fixed");
-                }
-            });
-        };
+        $(".header_area").addClass("navbar_fixed")
     };
     navbarFixed();
 	
